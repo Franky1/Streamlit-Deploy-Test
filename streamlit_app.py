5df2e0b64c409c9b9608ae685c4db8f360bc8e7c
Arvid Teichtmann <arvid.teichtmann@gmx.de>
2021-04-16 17:30:19 +0200

    test pyodbc

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,6 @@
-import pickle
-import re
-
-import altair as alt
-import lightgbm
-import lime
-import lime.lime_tabular
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-import shap
 import streamlit as st
-import streamlit.components.v1 as components
-from lightgbm import LGBMClassifier
-# from lightgbm_with_simple_features import *
-from PIL import Image
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 
-st.set_page_config(page_title="Streamlit Deployment Test", page_icon='✅')
-st.title('🔨 Selenium Test for Streamlit Sharing Deployment')
+
+st.set_page_config(page_title="Streamlit Deployment Test", page_icon='🔨')
+st.title('🔨 Test for Streamlit Sharing Deployment')
+st.balloons()
