823e1bcf64809272e46abc662aae902896c12ce0
Arvid Teichtmann <arvid.teichtmann@gmx.de>
2021-04-16 17:41:29 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 
 st.set_page_config(page_title="Streamlit Deployment Test", page_icon='🔨')
-st.title('🔨 Test for Streamlit Sharing Deployment')
+st.title('🔨 Test Streamlit Sharing Deployment')
 st.balloons()
